d993de16aace24295971da44b78384e479483f2b
Markus Lång <markus.lang@aalto.fi>
2024-12-21 18:31:25 +0200

    Updated config

--- init.lua
+++ init.lua
@@ -10,10 +10,10 @@ vim.opt.relativenumber = true -- Show relative line numbers simultaneously
 -- Enable true colors
 vim.opt.termguicolors = true
 
--- Packer plugin setup
+-- Packer plugin setup and plugins -- 
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    
+
     -- Gruvbox Theme
     use {
         'ellisonleao/gruvbox.nvim',
@@ -27,6 +27,18 @@ require('packer').startup(function(use)
             vim.cmd([[colorscheme gruvbox]])
         end
     }
+    -- -- Second theme: Switch by commenting.
+    -- use {
+    -- "catppuccin/nvim",
+    -- as = "catppuccin",
+    -- config = function()
+    --     require("catppuccin").setup({
+    --         transparent_background = true,
+    --         flavour = "mocha", -- Can be: latte, frappe, macchiato, mocha
+    --     })
+    --     vim.cmd.colorscheme "catppuccin"
+    -- end
+    -- }
     
     -- Auto-pairs
     use {
@@ -105,14 +117,23 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Setup C/C++ LSP (clangd)
 require('lspconfig').clangd.setup({
     capabilities = capabilities,
+    
     cmd = {
-        "clangd",
+        "clangd",  
+        --  note that clangd has to be installed with system package manager.
+        --  "/opt/homebrew/opt/llvm/bin/clangd", for MacOS, also add to path.      
+        --  brew install llvm
+        --  export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+        --  which clangd  # Should show /opt/homebrew/opt/llvm/bin/clangd
+        --  :MasonUninstall clangd
+        --  
         "--background-index",
         "--clang-tidy",
         "--header-insertion=iwyu",
         "--completion-style=detailed",
-        "--function-arg-placeholders"
+        "--function-arg-placeholders",
     },
+
     on_attach = function(client, bufnr)
         -- Enable completion triggered by <c-x><c-o>
         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -139,10 +160,39 @@ require('lspconfig').pyright.setup({
     end,
 })
 
+require('lspconfig').glsl_analyzer.setup({
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        -- Same keybindings as other LSPs
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    end,
+    filetypes = { "glsl", "vert", "frag", "geom", "comp" },  -- supported file extensions
+    cmd = { "glsl_analyzer" }  -- make sure this is installed via Mason
+})
+
+-- Add filetype detection for GLSL files
+vim.filetype.add({
+    extension = {
+        vert = 'glsl',
+        frag = 'glsl',
+        geom = 'glsl',
+        comp = 'glsl'
+    }
+})
 -- Telescope setup and keymaps
 require('telescope').setup({
     defaults = {
-        file_ignore_patterns = { "node_modules", ".git" },
+        file_ignore_patterns = { 
+            "node_modules",
+            ".git",
+            "build/.*",      -- Modified this line
+            "*/build/.*",    -- Added this to catch build dirs in subdirectories
+            "build",         -- Added this to catch the build dir itself
+        },
         mappings = {
             i = {
                 ["<C-j>"] = "move_selection_next",
@@ -153,7 +203,19 @@ require('telescope').setup({
 })
 
 -- Telescope keymaps
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set('n', '<space>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<space>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<space>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<space>fh', '<cmd>Telescope help_tags<cr>')
+
+
+-- Add this somewhere after your plugin setup
+require('nvim-treesitter.configs').setup({
+    ensure_installed = { "lua", "python", "cpp", "glsl" },  -- add to your existing list
+    highlight = {
+        enable = true,
+    },
+})
+
+
+
